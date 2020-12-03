32b3cc6c530b23eb70571dee3128ac56b9506cf7
komo-fr <komo.mdrms@gmail.com>
2020-12-04 00:22:21 +0900

    [Ac] 最長共通部分列

--- answer/ALDS_10_C/main.py
+++ answer/ALDS_10_C/main.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-
 
 def calc_max_lcs(X: str, Y: str, dp) -> int:
-    max_lcs = -float("inf")
-    for j in range(len(Y) + 1):
-        for i in range(len(X) + 1):
-            if i == 0 or j == 0:
-                lcs = 0
+    max_lcs = 0
+    pre_row = dp[0]
+    Y = " " + Y
+    X = " " + X
+    for j, YY in enumerate(Y):
+        if j == 0:
+            continue
+        row = dp[j]
+        for i, XX in enumerate(X):
+            if i == 0:
+                continue
+            if XX == YY:
+                # 末尾が一致していたら左斜め上+1
+                pre_lcs = pre_row[i - 1]
+                row[i] = pre_lcs + 1
+                if pre_lcs + 1 > max_lcs:
+                    max_lcs = pre_lcs + 1
             else:
-                if X[i - 1] == Y[j - 1]:
-                    # 末尾が一致していたら左斜め上+1
-                    lcs = dp[j - 1][i - 1] + 1
+                # 上と左
+                top, left = pre_row[i], row[i - 1]
+                if top > left:
+                    row[i] = top
                 else:
-                    # 上と左
-                    lcs = max([dp[j - 1][i], dp[j][i - 1]])
-            dp[j][i] = lcs
-            max_lcs = max([lcs, max_lcs])
+                    row[i] = left
+        pre_row = row
     return max_lcs
 
 
-for i in range(N):
-    X = input()
-    Y = input()
-    dp_table = [[0] * (len(X) + 2) for _ in range(len(Y) + 2)]
-    print(calc_max_lcs(X, Y, dp_table))
+if __name__ == "__main__":
+
+    N = int(input().split()[0])
+    for i in range(N):
+        X = input()
+        Y = input()
+        dp_table = [[0] * (len(X) + 2) for _ in range(len(Y) + 2)]
+
+        print(calc_max_lcs(X, Y, dp_table))
