d13abab272eaf87729acddd8e3f76f1625e33076
komo-fr <komo.mdrms@gmail.com>
2020-12-13 00:02:21 +0900

    [AC] 最長増加部分列

--- answer/DPL_1_D/main.py
+++ answer/DPL_1_D/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import bisect
 
 N = int(input().split()[0])
 a_list = []
@@ -6,15 +7,20 @@ a_list = []
 for _ in range(N):
     a = int(input().split()[0])
     a_list.append(a)
-dp = []
-max_dp_v = 1
-for i, a in enumerate(a_list):
-    max_v = 1
-    for j in range(i):
-        if a_list[j] < a:
-            max_v = max([max_v, dp[j] + 1])
-    dp.append(max_v)
-    max_dp_v = max([max_v, max_dp_v])
+
+dp = [float("inf") for i in range(N)]
+max_len = 1
+
+for j, a in enumerate(a_list):
+    if j == 0:
+        dp[0] = a
+        continue
+    i = bisect.bisect_right(dp, a)
+    # print(f"{i=}, {a=}, {dp=}")
+    if i == 0 or (i != 0 and a != dp[i - 1]):
+        dp[i] = min([a, dp[i]])
+        max_len = max([max_len, i + 1])
+
 # print(dp)
-ans = max_dp_v
+ans = max_len
 print(ans)
